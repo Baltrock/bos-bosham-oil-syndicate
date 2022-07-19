1a4f9826a309fae93d668e38c65d7c191e97c07e
Baltrock <bengmmurphy@hotmail.com>
2022-07-19 13:05:07 +0100

    please work

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,14 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "information_form", to: "information_form#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :information_form do
+    collection do
+      get :top
+    end
+  end
 end
