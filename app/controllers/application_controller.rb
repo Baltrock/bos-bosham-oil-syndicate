e2fb33cee4545fea023364accd0417abedc2f870
Baltrock <bengmmurphy@hotmail.com>
2022-07-27 15:16:44 +0100

    getting there

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(user)
     stored_location_for(resource) ||
-     if user.admin?
+     if user.is_admin?
        admin_root_path
      else
        user_root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # validates :email, presence: true
-  validates :is_admin, inclusion: [true, false]
+  validates :is_admin, inclusion: [true]
   validates :email, format: { with: /\A.*@.*\.com\z/ }
   validates :password, uniqueness: false
   has_many :information_form
